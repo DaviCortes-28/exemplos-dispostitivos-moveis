3f125539cb3c2e0956d1418f32ccd8f305217688
Leandro Foly <l.foly@gsuite.iff.edu.br>
2026-08-31 13:37:02 -0300

    atualização teste

--- exemplo_simples_bd/lib/listagem.dart
+++ exemplo_simples_bd/lib/listagem.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+//comentario
 class Listagem extends StatefulWidget {
   const Listagem({super.key});
 
